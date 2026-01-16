c50099a12086020981a8a63e48bb9983f209c5f8
luozguo <anitadou9@gmail.com>
2026-01-16 21:02:31 +0100

    clarified ownership

--- init.c
+++ init.c
@@ -1,13 +1,13 @@
-#include "fracto_header.h"
 #include "MLX42.h"
+#include "fracto_header.h"
 
 // set the initial view for the fractal. Now only for Mandelbrot
 void	set_view(t_context *ctx)
 {
-	ctx->view.x_min = -2.0;
-	ctx->view.x_max = 1.0;
-	ctx->view.y_min = -1.5;
-	ctx->view.y_max = 1.5;
+	ctx->view.re_min = -2.0;
+	ctx->view.re_max = 1.0;
+	ctx->view.im_min = -1.5;
+	ctx->view.im_max = 1.5;
 }
 
 void	init_context(t_context *ctx)
@@ -15,19 +15,18 @@ void	init_context(t_context *ctx)
 	ctx->mlx = mlx_init(WIDTH, HEIGHT, "fractol", false);
 	if (!ctx->mlx)
 		exit(EXIT_FAILURE);
-
-	ctx->img = mlx_new_image(ctx->mlx, WIDTH, HEIGHT);
-	if (!ctx->img)
+	ctx->img.mlx_img = mlx_new_image(ctx->mlx, WIDTH, HEIGHT);
+	if (!ctx->img.mlx_img)
 	{
 		mlx_terminate(ctx->mlx);
 		exit(EXIT_FAILURE);
 	}
-
-	if (mlx_image_to_window(ctx->mlx, ctx->img, 0, 0) < 0)
+	if (mlx_image_to_window(ctx->mlx, ctx->img.mlx_img, 0, 0) < 0)
 	{
-		mlx_delete_image(ctx->mlx, ctx->img);
+		mlx_delete_image(ctx->mlx, ctx->img.mlx_img);
 		mlx_terminate(ctx->mlx);
 		exit(EXIT_FAILURE);
 	}
 	ctx->max_iter = 100;
+	set_view(ctx);
 }
