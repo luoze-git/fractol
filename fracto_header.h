3c8cb9dda15213d7cba486cd0a69ee8a2773b208
luozguo <anitadou9@gmail.com>
2026-01-16 15:30:31 +0100

    conceptully built for the core flow

---
+++ fracto_header.h
@@ -0,0 +1,51 @@
+# ifndef FRACTO_HEADER_H
+#define FRACTO_HEADER_H
+
+// big context
+typedef struct s_context
+{
+    void *mlx;
+    void *win;
+
+    t_img img; 
+    t_view view;
+
+    int max_iter;
+} t_context;
+
+// a view of the chosen fractal
+typedef struct s_view
+{
+    double x_min;
+    double x_max;
+    double y_min;
+    double y_max;
+} t_view;
+
+// image
+typedef struct s_img
+{
+	void	*img;
+	char	*addr_pix;
+	int		bpp_bits;
+	int		line_len_bytes;
+	int		endian_dummy;
+}	t_img;
+
+// coordinates
+typedef struct s_complex
+{
+	double	re;
+	double	im;
+}	t_complex;
+
+// meaningfully init struct
+void init_context (t_context *ctx);
+
+// math part
+void mandelbrot_iter_math(t_complex c , int max_iter , int *out_iter);
+int	escape_check(t_complex z);
+
+// paint according to args
+void	put_pixel(t_img *img, int x, int y, int color);
+# endif // FRACTO_HEADER_H
